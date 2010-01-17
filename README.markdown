0d82799bac9c343a588e11430eec66f0b5ebe764
remi <remi@remitaylor.com>
2010-01-17 13:18:53 -0700

    renamed middleware to Rack::Payment and simplified specs ... starting with high-level integration specs.  my specs were too low level.  we'll nail down the functionality then make it easy to configure / override defaults

--- README.markdown
+++ README.markdown
@@ -1,44 +1,37 @@
-RActiveMerchant
-===============
+Rack::Payment
+=============
 
-RActiveMerchant = Rack + ActiveMerchant
+{Rack::Payment} is a [Rack][] middleware for requiring single payments 
+from your Ruby web applications.
 
-Ideas
+We've purposefully called this Rack Payment, as opposed to Rack Payment**s**, 
+because this middleware has no concept of multiple / recurring payments.
+
+This simply makes it easy to collect individual payments, eg:
+
+ * User adds some items to a shopping card
+ * User clicks 'Checkout'
+ * User fills out their billing information
+ * User gets billed!
+
+Ok, so what is this *really*?
+-----------------------------
+
+Really, this is just a Rack API / wrapper for [ActiveMerchant][].
+
+With this, you can add 1 line of code to your web application and you'll get:
+
+ * A page that lets a user fill out their billing information
+ * A confirmation page, if the user's payment goes through 
+ * Error messages, if the user's payment doesn't go through
+
+Of course, it's very easy to override these pages yourself!
+
+Usage
 -----
 
-This isn't complete yet, so here are merely some ideas ...
+Coming soon!  Gotta make the specs pass first ...
 
-All paths overridable!
 
-GET /ractivemerchant/checkout
-
-    A UI for filling out credit card payment, etc.
-    You need to pass it the amount you want to buy.
-    You can pass it text/info to display, maybe?
-    KISS because you can override this view!
-
-POST /ractivemerchant/purchase
-
-    This actually does the purchase.
-
-    If it works, it redirects to /ractivemerchant/complete.
-
-    If it doesn't, it redirects BACK (eg. back to /checkout)
-    providing details on why it didn't work.
-
-env['ractivemerchant'] will be used for passing data within single requests.
-
-For instance, after a POST to /purchase, you'll be able to get the actual 
-ActiveMerchant response object(s) via env['ractivemerchant'], if you want them.
-
-All of the env variables can be accessed more easily via helper methods that 
-can easily be included via a module.  We'll copy what we did with Rack::OAuth.
-
-Also, it should be really easy to write your integration specs with this.  
-If people use a BogusGateway, everything should "just work."
-
-Example:
-
-    gateway = ActiveMerchant::Billing::Base.gateway('paypal').new({ ... })
-
-    use RActiveMerchant, gateway, :checkout_path => '/foo', :purchase_path => '/bar'
+[rack]:           http://rack.rubyforge.org
+[activemerchant]: http://www.activemerchant.org
---
+++ spec/rack-payment_spec.rb
@@ -0,0 +1,111 @@
+require File.dirname(__FILE__) + '/../lib/rack/payment'
+%w( rubygems spec rack/test webrat fakeweb sinatra/base ).each {|lib| require lib }
+
+Webrat.configure do |config|
+  config.mode = :rack
+end
+
+FakeWeb.allow_net_connect = false # just incase ActiveMerchant tries connecting ...
+
+# Rack::Test doesn't give you a way to change the application  Oo
+module Rack::Test::Methods
+  def build_rack_mock_session
+    Rack::MockSession.new(@_rack_app || app)
+  end
+  def set_rack_app app
+    @_rack_app = app
+    @_rack_mock_sessions = { :default => build_rack_mock_session }
+  end
+end
+
+describe Rack::Payment do
+
+  # for integration testing
+  include Rack::Test::Methods
+  include Webrat::Methods
+  include Webrat::Matchers
+
+  describe 'configuration' do
+
+    before do
+      @app     = lambda { }
+      @gateway = ActiveMerchant::Billing::BogusGateway.new
+    end
+
+    it 'requires a valid Rack application (that responds to #call)' do
+      lambda { Rack::Payment.new               }.should raise_error(ArgumentError, /wrong number of arguments \(0 for 2\)/)
+      lambda { Rack::Payment.new nil, @gateway }.should raise_error(ArgumentError, /valid rack app/i)
+
+      Rack::Payment.new(@app, @gateway).app.should == @app
+    end
+
+    it 'requires gateway (that responds to #purchase)' do
+      lambda { Rack::Payment.new @app      }.should raise_error(ArgumentError, /wrong number of arguments \(1 for 2\)/)
+      lambda { Rack::Payment.new @app, nil }.should raise_error(ArgumentError, /valid gateway/i)
+
+      Rack::Payment.new(@app, @gateway).gateway.should == @gateway
+    end
+
+  end
+
+  describe 'high level integration' do
+
+    it 'should be able to make a successful purchase (MOST IMPORTANT)' do
+      pending 'refactoring, then coming back to this'
+      class SimpleApp < Sinatra::Base
+        use Rack::Payment, ActiveMerchant::Billing::BogusGateway.new
+
+        get '/' do
+          %{
+            <form action='/' method='post'>
+              <input type='text' id='monies' name='monies' />
+              <input type='submit' value='Checkout' />
+            </form>
+          }
+        end
+
+        post '/' do
+          [ 402, { 'Payment-Amount' => "#{ params[:monies] } USD" }, ['Payment Required'] ] # 402 Payment Required:w
+        end
+      end
+
+      set_rack_app SimpleApp.new
+
+      # visit some fake page where we 'checkout' from to start the middleware process!
+      visit '/'
+      fill_in :monies, :with => 9.95
+      click_button 'Checkout'
+
+      # this should take us to either the app's screen or the checkout screen we supply out of the box      
+
+      # credit card
+      { 
+        :first_name       => 'remi',
+        :last_name        => 'taylor',
+        :number           => '1',     # 1 is valid using the BogusGateway
+        :cvv              => '123',
+        :expiration_month => '01',
+        :expiration_year  => '2015' 
+      }.each { |key, value| fill_in "credit_card_#{key}", :with => value.to_s }
+
+      # billing address
+      { 
+        :name     => 'remi',
+        :address1 => '123 Chunky Bacon St.',
+        :city     => 'Magical Land',
+        :state    => 'NY',
+        :country  => 'US',
+        :zip      => '12345'
+      }.each { |key, value| fill_in "billing_address_#{key}", :with => value.to_s }
+
+      click_button 'Purchase'
+
+      # if all went well, we should be on the order confirmation page ... the app's or one in the middleware
+
+      last_response.should include('Order successful')
+      last_response.should include('9.95')
+    end
+
+  end
+
+end
