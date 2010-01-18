7b72cdacc3f962e5f451aab77de315a6aaa495be
remi <remi@remitaylor.com>
2010-01-18 05:13:12 -0700

    on_success example can be run

--- examples/on_success-override.rb
+++ examples/on_success-override.rb
@@ -1,24 +1,33 @@
-# 
+require 'rubygems'
+require 'sinatra/base'
+require 'haml'
+require File.dirname(__FILE__) + '/../lib/rack/payment' unless defined?(Rack::Payment)
+
+ActiveMerchant::Billing::Base.mode = :test
+
+# .gateway.rb should be set in your root path and it should 
+# specify an ActiveMerchant::Billing::Gateway with the 
+# constant GATEWAY
+require File.dirname(__FILE__) + '/../.gateway' unless ENV['RACK_ENV'] == 'test'
+
+# Overrides on_success with its own page
 class SimpleAppWithOnSuccessOverridden < Sinatra::Base
 
+  # For our specs, we need access to this gateway instance
   class << self; attr_accessor :gateway; end
-  @gateway = ActiveMerchant::Billing::BogusGateway.new
+  @gateway = ENV['RACK_ENV'] == 'test' ? ActiveMerchant::Billing::BogusGateway.new : Kernel.const_get(:GATEWAY)
 
   use Rack::Session::Cookie
-
   use Rack::Payment, gateway, :on_success => '/custom_success_page'
+
+  use_in_file_templates!
 
   helpers do
     include Rack::Payment::Methods
   end
 
   get '/' do
-    %{
-      <form action='/' method='post'>
-        <input type='text' id='monies' name='monies' />
-        <input type='submit' value='Checkout' />
-      </form>
-     }
+    haml :index
   end
 
   post '/' do
@@ -27,8 +36,39 @@ class SimpleAppWithOnSuccessOverridden < Sinatra::Base
   end
 
   get '/custom_success_page' do
-    "w00t!  Success!  You should have been charged #{ payment.amount } (#{ payment.amount_in_cents }).  " + 
-    "Raw capture response: #{ payment.capture_response.inspect }.  " + 
-    "Raw authoriziation request: #{ payment.authorize_response.inspect }"
+    haml :on_success
   end
 end
+
+__END__
+
+@@ on_success
+
+%h2  w00t!  Success!
+
+%p== You should have been charged #{ payment.amount } (#{ payment.amount_in_cents })
+
+%p Raw capture response:
+%pre~ payment.capture_response.inspect
+
+%p Raw authoriziation request:
+%pre~ payment.authorize_response.inspect
+
+@@ index
+
+%form{ :action => '/', :method => 'post' }
+  %label
+    How many monies would you like to spend?
+    %input{ :type => 'text', :id => 'monies', :name => 'monies' }
+  %input{ :type => 'submit', :value => 'Checkout' }
+
+@@ layout
+
+!!! XML
+!!! Strict
+%html
+  %head
+    %title Rack::Payment Sample (on_success override)
+  %body
+    %h1 Rack::Payment Sample (on_success override)
+    #content= yield
---
+++ examples/on_success-override.ru
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + '/on_success-override'
+run SimpleAppWithOnSuccessOverridden.new
