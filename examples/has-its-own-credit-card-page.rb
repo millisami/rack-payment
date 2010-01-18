3a02c9c63ee69be2bf5c9e339f4b17d139d3f180
remi <remi@remitaylor.com>
2010-01-18 05:25:15 -0700

    has-its-own-credit-card-page specs work but it's not working properly when I try testing it ... need to try to reproduce the bug in the specs to fix it

--- examples/has-its-own-credit-card-page.rb
+++ examples/has-its-own-credit-card-page.rb
@@ -1,36 +1,38 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'haml'
+require File.dirname(__FILE__) + '/../lib/rack/payment' unless defined?(Rack::Payment)
+
+ActiveMerchant::Billing::Base.mode = :test
+
+# .gateway.rb should be set in your root path and it should 
+# specify an ActiveMerchant::Billing::Gateway with the 
+# constant GATEWAY
+require File.dirname(__FILE__) + '/../.gateway' unless ENV['RACK_ENV'] == 'test'
+
+# Has its own page with CC / Billing info
 class SimpleAppWithOwnCreditCardPage < Sinatra::Base
 
+  # For our specs, we need access to this gateway instance
   class << self; attr_accessor :gateway; end
-  @gateway = ActiveMerchant::Billing::BogusGateway.new
+  @gateway = ENV['RACK_ENV'] == 'test' ? ActiveMerchant::Billing::BogusGateway.new : Kernel.const_get(:GATEWAY)
 
   use Rack::Session::Cookie
-
   use Rack::Payment, gateway
+
+  use_in_file_templates!
 
   helpers do
     include Rack::Payment::Methods
   end
 
   get '/' do
-    html = "<h1>Custom Page</h1>"
-    unless payment.errors.empty?
-      html += "<p>#{ payment.errors.join(', ') }</p>"
-    end
-    html += "<form action='/' method='post'>"
-    html += "<input type='text' id='monies' name='monies' value='#{ payment.amount }' />"
-    %w( first_name last_name number cvv expiration_month expiration_year type ).each do |field|
-      full_field = "credit_card[#{field}]"
-      html += "<input type='text' name='#{full_field}' value='#{ payment.credit_card[field] }' />"
-    end
+    haml :index
+  end
 
-    %w( name address1 city state country zip ).each do |field|
-      full_field = "address[#{ field }]"
-      html += "<input type='text' name='#{full_field}' value='#{ payment.billing_address[field] }' />"
-    end
-    html += "<input type='submit' value='Purchase' />"
-    html += "</form>"
-
-    html
+  get '/styles.css' do
+    content_type 'text/css'
+    sass :styles
   end
 
   post '/' do
@@ -41,3 +43,54 @@ class SimpleAppWithOwnCreditCardPage < Sinatra::Base
     [ 402, {}, ['Payment Required'] ]
   end
 end
+
+__END__
+
+@@ index
+
+%h1 Custom Page
+
+- unless payment.errors.empty?
+  %p= payment.errors.join(', ')
+
+%form{ :action => '/', :method => 'post' }
+
+  %label
+    How many monies would you like to spend?
+    %input{ :type => 'text', :id => 'monies', :name => 'monies', :value => payment.amount }
+
+    - %w( first_name last_name number cvv expiration_month expiration_year type ).each do |field|
+      - full_field = "credit_card[#{field}]"
+      %label
+        = field.gsub('_', ' ').capitalize
+        %input{ :type => 'text', :name => full_field, :value => payment.credit_card[field] }
+
+    - %w( name address1 city state country zip ).each do |field|
+      - full_field = "address[#{ field }]"
+      %label
+        = field.gsub('_', ' ').capitalize
+        %input{ :type => 'text', :name => full_field, :value => payment.billing_address[field] }
+  
+  %input{ :type => 'submit', :value => 'Purchase' }
+
+@@ layout
+
+!!! XML
+!!! Strict
+%html
+  %head
+    %title Rack::Payment Sample
+    %link{ :rel => 'stylesheet', :type => 'text/css', :href => '/styles.css' }
+  %body
+    %h1 Rack::Payment Sample
+    #content= yield
+    
+@@ styles
+
+body
+  :background-color #666
+  :color            white
+
+  form
+    input
+      :display block
---
+++ examples/has-its-own-credit-card-page.ru
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + '/has-its-own-credit-card-page'
+run SimpleAppWithOwnCreditCardPage.new
