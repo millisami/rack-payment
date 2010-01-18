4520ff04c97267d61888edeb445e7edd053c7e25
remi <remi@remitaylor.com>
2010-01-17 18:10:39 -0700

    cleaning up Rack::Purchase::Request.  it needs a lot of work, but it should start to get easier to work with.

--- lib/rack-payment/request.rb
+++ lib/rack-payment/request.rb
@@ -6,64 +6,68 @@ module Rack     #:nodoc:
       extend Forwardable
 
       def_delegators :payment_instance, :app, :gateway, :on_success
+      def_delegators :request, :params
 
       # raw ENV hash
       attr_accessor :env
 
       # Rack::Request
-      attr_accessor :req
+      attr_accessor :request
+
+      # Rack::Response that results from calling the Rack application
+      attr_accessor :app_response
 
       def payment_instance
         env['rack.payment']
       end
 
-      def initialize env
-        self.env = env
-        self.req = Rack::Request.new @env
+      def payment
+        env['rack.payment.data'] ||= Rack::Payment::Data.new
       end
 
-      # the final rack response
+      def amount_in_session
+        env['rack.session']['rack.payment'] ||= {}
+        env['rack.session']['rack.payment']['amount']
+      end
+
+      def amount_in_session= value
+        env['rack.session']['rack.payment'] ||= {}
+        env['rack.session']['rack.payment']['amount'] = value
+      end
+
+      # ...
+      def initialize env
+        self.env          = env
+        self.request      = Rack::Request.new @env
+
+        raw_rack_response = app.call env
+        self.app_response = Rack::Response.new raw_rack_response[2], raw_rack_response[0], raw_rack_response[1]
+      end
+
+      # The final rack response.  This "runs" the request.
       def finish
-        request = Rack::Request.new(env)
+        if app_response.status == 402 # Payment Required
 
-        raw_response = app.call env
-        app_response = Rack::Response.new raw_response[2], raw_response[0], raw_response[1]
-
-        if app_response.status == 402
-
-          payment = env['rack.payment.data'] # data put into ENV
-
-          # check for payment.amount ... should blow up if not set
-          env['rack.session']['rack.payment'] ||= {}
-          env['rack.session']['rack.payment']['amount'] = payment.amount
+          self.amount_in_session = payment.amount # we need to put this in the session ... i forget why ...
 
           if payment.card_or_address_partially_filled_out?
-
-            # You've filled stuff out!  Try to process ...
-            return process_credit_card(env)
+            return process_credit_card # You've filled stuff out!  Try to process ...
           else
-
-            # Payment Required!
-            return credit_card_and_billing_info_response env
+            return credit_card_and_billing_info_response # Payment Required!
           end
 
         elsif request.path_info == '/rack-payment-processing'
-
-          # Try to process the request
-          return process_credit_card(env)
-
+          return process_credit_card # Try to process the request
         end
 
-        app_response.finish
+        app_response.finish # default to returning the application's response
       end
 
-      def process_credit_card env
-        env['rack.payment.data'] ||= Rack::Payment::Data.new
-        payment = env['rack.payment.data']
-        payment.amount ||= env['rack.session']['rack.payment']['amount'] # sometimes we get the amount from the session ...
+      def process_credit_card
+        payment.amount ||= amount_in_session
 
         unless payment.card_or_address_partially_filled_out?
-          Rack::Request.new(env).params.each do |field, value|
+          params.each do |field, value|
             if field =~ /^credit_card_(\w+)/
               payment.credit_card.update $1 => value
             elsif field =~ /billing_address_(\w+)/
@@ -89,35 +93,28 @@ module Rack     #:nodoc:
             end
 
           else
-            credit_card_and_billing_info_response env, [payment.authorize_response.message]
+            credit_card_and_billing_info_response [payment.authorize_response.message]
           end
 
         else
-          credit_card_and_billing_info_response env, payment.credit_card.errors
+          credit_card_and_billing_info_response payment.credit_card.errors
         end
       end
 
-      def credit_card_and_billing_info_response env, errors = nil
+      def credit_card_and_billing_info_response errors = nil
         html = ''
-
-        params = Rack::Request.new(env).params
-
         if errors and not errors.empty?
           html += '<p>' + errors.join(', ') + '</p>'
         end
-
         html += "<form action='/rack-payment-processing' method='post'>"
-
         %w( first_name last_name number cvv expiration_month expiration_year type ).each do |field|
           full_field = "credit_card_#{field}"
           html += "<input type='text' name='#{full_field}' value='#{ params[full_field] }' />"
         end
-
         %w( name address1 city state country zip ).each do |field|
           full_field = "billing_address_#{ field }"
           html += "<input type='text' name='#{full_field}' value='#{ params[full_field] }' />"
         end
-
         html += "<input type='submit' value='Purchase' />"
         html += "</form>"
         
