4e58e15e7540a3346641651286edc1fadc24bbf1
remi <remi@remitaylor.com>
2010-01-29 19:28:15 -0700

    added autocomplete='off' to the credit card number/cvv fields

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require File.dirname(__FILE__) + '/../lib/rack/payment/test'
 %w( rubygems spec rack/test webrat fakeweb sinatra/base tempfile ).each {|lib| require lib }
 
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each     {|support| require support }
-Dir[File.dirname(__FILE__) + '/../examples/**/*.rb'].each {|example| require example }
+Dir[File.dirname(__FILE__) + '/../examples/*.rb'].each {|example| require example }
 
 # Log everything, just for kicks
 FileUtils.rm_f(File.dirname(__FILE__) + '/rack-payment-specs.log')
